4bdb637ec51531ad41e7226d4e50f18298e3b389
huynqhttt <huy.nguyen@nfq.asia>
2019-05-17 18:22:58 +0700

    Clone react native intent launcher for android

--- index.js
+++ index.js
@@ -1,15 +1,7 @@
-// @flow
-
-'use strict';
-
-const RNI18n = require('react-native').NativeModules.RNI18n;
-const I18nJs = require('i18n-js');
-
-if (typeof RNI18n !== 'undefined') {
-  I18nJs.locale = RNI18n.languages[0];
-} else if (__DEV__) {
-  console.warn('react-native-i18n module is not correctly linked');
+import { NativeModules } from 'react-native'
+import React from 'react'
+import IntentConstant from './IntentConstant'
+export default NativeModules.IntentLauncher
+export {
+	IntentConstant
 }
-
-export const getLanguages = () => RNI18n.getLanguages();
-export default I18nJs;
